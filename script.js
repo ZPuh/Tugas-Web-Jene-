d47a19a656fbacf62ad76afe6b1dff312cc81ca9
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-12-01 16:12:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Inisialisasi Swiper.js
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
