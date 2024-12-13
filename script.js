a5018b5334af6d7620a06c9d3f9c671cb7c49733
Muhammad Zuhdi <146620601+ZPuh@users.noreply.github.com>
2024-12-14 00:18:14 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-// Inisialisasi Swiper.js
-const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
+ // Inisialisasi Swiper.js
+ const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// Hamburger Menu Toggle
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
